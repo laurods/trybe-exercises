317efd60be16ecad503f1c0d258647a8dbbf971e
LAURO DE SA <itrafor@gmail.com>
2020-08-27 19:33:28 -0300

    Concluidos até exercicio 9

---
+++ introduction/bloco_4/exercises_4.1/pro6_maior.js
@@ -0,0 +1,7 @@
+let a=8;
+let b=2;
+if(a>b){
+console.log("a é maior que b");
+}else{
+    console.log("a não é maior que b");
+}
---
+++ introduction/bloco_4/exercises_4.1/pro9.js
@@ -0,0 +1,9 @@
+let a=45;
+let b=45;
+let c=90;
+if((a+b+c)==180){
+console.log("São medidas de angulos do triangulo");
+}
+else{
+    console.log("Não são medidas de angulo do triangulo")
+}
