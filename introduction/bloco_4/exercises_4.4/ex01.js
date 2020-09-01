bac31449432390f35751360a51c6343347b2cdf5
LAURO DE SA <itrafor@gmail.com>
2020-09-01 15:10:56 -0300

    Exercicio 1 concluido

---
+++ introduction/bloco_4/exercises_4.4/ex01.js
@@ -0,0 +1,7 @@
+let info = {
+    personagem: "Margarida",
+    origem:"Pato Donald",
+    nota:"Namorada do personagem principal dos quadrinhos do Pato Donald",
+};
+
+console.log('Bem vinda '+info.personagem+'');
